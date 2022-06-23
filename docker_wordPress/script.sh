ed8b54e1667e291b6f4640800200e431170c4a0c
Marc-Etienne Bonnet <webbonnet@gmail.com>
2022-06-23 14:55:55 +0200

    correction de l ordre d execution des docker via healthcheck

--- docker_wordPress/script.sh
+++ docker_wordPress/script.sh
@@ -1,3 +1,3 @@
 
-wp core install --allow-root --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$MYSQL_USER --admin_pass=$MYSQL_PASSWORD --admin_email=$WORDPRESS_EMAIL
+wp core install --allow-root --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=$WORDPRESS_EMAIL
 exec php-fpm7.3 -F
